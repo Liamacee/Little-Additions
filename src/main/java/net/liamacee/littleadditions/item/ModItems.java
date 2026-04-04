2a84056c354ad9c46075ed1cb83b4f7a1860267a
Liamacee <liamchabon722@gmail.com>
2026-04-05 01:34:10 +0200

    Changed the fuel handling to use datapacks instead

--- src/main/java/net/liamacee/littleadditions/item/ModItems.java
+++ src/main/java/net/liamacee/littleadditions/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.liamacee.littleadditions.item;
 
 import net.liamacee.littleadditions.LittleAdditions;
-import net.liamacee.littleadditions.item.custom.FuelItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -11,9 +10,9 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(LittleAdditions.MOD_ID);
 
     public static final DeferredItem<Item> TINY_COAL = ITEMS.register("tiny_coal",
-            () -> new FuelItem(new Item.Properties(), 200));
+            () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> TINY_CHARCOAL = ITEMS.register("tiny_charcoal",
-            () -> new FuelItem(new Item.Properties(), 200));
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
